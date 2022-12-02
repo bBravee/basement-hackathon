3e350c8e0eff9cb04029faa901485cadf412fd7f
bBravee <jakub.kusmierczyk@op.pl>
2022-12-02 19:46:23 +0100

    navbar commit

---
+++ view/src/components/Navbar.js
@@ -0,0 +1,23 @@
+import React from "react"
+import Logo from "./../images/corner_logo.png"
+
+export default function Navbar() {
+    return (
+        <nav className="nav-wrapper">
+            <div className="nav-img-wrapper">
+                <img className="nav-logo" src={Logo} alt="ikona"></img>
+                <h2>Kicker Championships</h2>
+            </div>
+            <ul className="nav-ol">
+                <li>
+                    <button type="button" class="btn btn-outline-warning btn-lg">Rejestracja</button>
+                </li>
+                <li>
+                    <button type="button" class="btn btn-outline-warning btn-lg">Zaloguj</button>
+                </li>
+            </ul>
+        </nav>
+    )
+
+
+}
