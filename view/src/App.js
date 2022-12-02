d7fba69fa90bbba0f11321d290391ad9fc831d55
BartlomiejCzech <choocas0@gmail.com>
2022-12-02 20:14:20 +0100

    Footer here
    Footer here

--- view/src/App.js
+++ view/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import MainSection from './components/MainSection.js';
+import Footer from './components/Footer.js';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <MainSection/>
+      <p>React</p>
+      <Footer/>
     </div>
   );
 }
