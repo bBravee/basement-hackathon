deda9d036e59e2e7cef327d691dc1fe006a3c05a
Michał Rusak <104869035+michalrusak@users.noreply.github.com>
2022-12-03 07:38:01 +0100

    Node modules

---
+++ view/src/components/Testing.js
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import "./styles/testing.css";
+import {Tournament} from "react-tournament-ready";
+
+export default function Testing() {
+  const [score, setScore] = useState(null);
+
+  const handleSetNewScore = () => {
+    setScore({
+      score: 26
+    });
+
+    //score is an object with 1 key ('score')
+  };
+
+  return (
+    <div className="App123">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Edit to see some magic happen!</h2>
+      <Tournament
+        team={{
+          name: "team2",
+          id: "team2ID"
+        }}
+        teams={[
+          {
+            name: "team1",
+            id: "team1ID"
+          },
+          {
+            name: "team2",
+            id: "team2ID"
+          },
+          {
+            name: "team3",
+            id: "team3ID"
+          },
+          {
+            name: "team4",
+            id: "team4ID"
+          },
+          {
+            name: "team5",
+            id: "team5ID"
+          },
+          {
+            name: "team6",
+            id: "team6ID"
+          },
+          {
+            name: "team7",
+            id: "team7ID"
+          },
+          {
+            name: "team8",
+            id: "team8ID"
+          }
+        ]}
+        insertScore={score}
+      />
+
+      <button onClick={handleSetNewScore}>Set Score</button>
+    </div>
+  );
+}
--- view/src/components/TournamentInfo.js
+++ view/src/components/TournamentInfo.js
@@ -5,31 +5,36 @@ import './styles/TournamentInfo.css'
 
 const TournamentInfo = () => {
 
-    function generate () {
-        const wrapper = document.getElementById("wrapper");
-        var j = 0;
-        var x = 8;
+    // function generate () {
+    //     // const wrapper = document.getElementById("wrapper");
+    //     var j = 0;
+    //     var x = 8;
 
-        for(var i=0; i<=3; i++) {
-        // Tworzy nowy kontener
-        const newDiv = document.createElement("div");
-        // Dodaje unique id kontenerowi
-        newDiv.setAttribute("id", j++);
-        newDiv.classList.add("new-div");
-        // Dopina do konteneru nowy kontener
-        wrapper.appendChild(newDiv);
-        for(var k=x; k>0; k--) {
-            var neww = document.createElement("nav");
-            newDiv.appendChild(neww);
-        }
-        if(x === 2) {
-            x = 1;
-        } else {
-            x = x/2;
-        }
+    //     for(var i=0; i<=3; i++) {
+    //     // Tworzy nowy kontener
+    //     const newDiv = document.createElement("div");
+    //     // Dodaje unique id kontenerowi
+    //     newDiv.setAttribute("id", j++);
+    //     newDiv.classList.add("new-div");
+    //     // Dopina do konteneru nowy kontener
+    //     wrapper.appendChild(newDiv);
+    //     for(var k=x; k>0; k--) {
+    //         var neww = document.createElement("nav");
+    //         newDiv.appendChild(neww);
+    //     }
+    //     if(x === 2) {
+    //         x = 1;
+    //     } else {
+    //         x = x/2;
+    //     }
         
-        }
-    }
+    //     }
+    // }
+
+    // const teams = [1,2,3,4,5,6,7,8];
+    // const items = teams.map((team,index)=> {
+
+    // })
  
     return ( 
         <>
@@ -37,8 +42,8 @@ const TournamentInfo = () => {
 
         <div className="tournamentInfo">
             <div className="back"> --- Back</div>
-            <div className="rounds" id="wrapper">
-                rounds
+            <div className="rounds">
+                {}
             </div>
             <div className="scores">scores</div>
         </div>
