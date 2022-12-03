31ab6aa80886df7a651d586d2cc50db22b0ec952
bBravee <jakub.kusmierczyk@op.pl>
2022-12-03 05:50:07 +0100

    fetch

--- view/src/components/LogInForm.js
+++ view/src/components/LogInForm.js
@@ -3,7 +3,23 @@ import './styles/LogInForm.css'
 import Logo from "./../images/corner_logo.png";
 
 
-export default function LogInForm() {
+export default  function LogInForm() {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const data = {
+            nickname: 'russak',
+            password: '123'
+        }
+
+        fetch('http://localhost:5000/api/v1/player/login', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json"},
+            body: JSON.stringify(data)
+        }).then(() => {
+            console.log('new data send');
+        })
+    }
+      
     return (
     <div className="login-wrapper">
         <form className="login-form">
@@ -16,8 +32,9 @@ export default function LogInForm() {
                 <input type="password" id="form2Example2" className="form-control" />
                 <label className="login-form-label" for="form2Example2">Password</label>
             </div>
-        <button type="button" class="btn btn-outline-warning btn-lg btn-block mb-4">Sign in</button>
+        <button type="button" onClick={handleSubmit} class="btn btn-outline-warning btn-lg btn-block mb-4">Sign in</button>
         </form>
     </div>
-    )
-}
+        )
+    }
+    
