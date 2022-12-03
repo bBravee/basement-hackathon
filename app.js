a7e44c2f11d5219acd0c0ca6a358576997718c89
bBravee <jakub.kusmierczyk@op.pl>
2022-12-03 05:54:01 +0100

    sadas

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(notFoundMiddleware)
 
 
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 const connectDB = require("./database/connect")
 const start = async ()=>{
     try {
